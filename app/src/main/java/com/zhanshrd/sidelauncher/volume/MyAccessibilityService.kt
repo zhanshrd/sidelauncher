34fbc5e9930891a6e5cc4dceaf327b03ca66a139
zhanshrd <zhanshrd@gmail.com>
2024-03-18 18:49:34 +0800

    volume bind key button fix

--- app/src/main/java/com/zhanshrd/sidelauncher/volume/MyAccessibilityService.kt
+++ app/src/main/java/com/zhanshrd/sidelauncher/volume/MyAccessibilityService.kt
@@ -27,28 +27,42 @@ class MyAccessibilityService : AccessibilityService() {
             if (event?.action == KeyEvent.ACTION_UP){
                 //Toast.makeText(this, "按下按钮", Toast.LENGTH_SHORT).show()
                 val audioManager: AudioManager = this.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-                // 检查按键事件按键码
-                if (event.keyCode == 131) {//音量加
-                    var alarmVolume = audioManager.getStreamVolume(AudioManager.STREAM_ALARM)
-                    if(alarmVolume>0){
-                        Toast.makeText(this, "音量加", Toast.LENGTH_SHORT).show()
-                        audioManager.adjustSuggestedStreamVolume(
-                            AudioManager.STREAM_MUSIC, // 要调整的音频流类型
-                            AudioManager.ADJUST_RAISE, // 调整方向：增加音量
-                            0 // 可选的标志位，通常设置为0
-                        )
+
+                var alarmVolume = audioManager.getStreamVolume(AudioManager.STREAM_ALARM)
+                var musicVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+
+                if (musicVolume > 0 && alarmVolume == 0){
+                    //skip
+                }
+                else{
+                    var targetAlarmVolume = alarmVolume
+                    var targetMusicVolume = musicVolume
+
+                    // 检查按键事件按键码
+                    if (event.keyCode == 131) {//音量加
+                        //音量范围控制
+                        if(targetAlarmVolume<30){
+                            targetAlarmVolume++
+                        }
+                        if(targetMusicVolume<30){
+                            targetMusicVolume++
+                        }
+                        //Toast.makeText(this, "音量加", Toast.LENGTH_SHORT).show()
+                        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, targetAlarmVolume, 0)
+                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, targetMusicVolume, 0)
                         //return true // 如果事件已经被处理，返回 true
                     }
-                }
-                else if (event.keyCode == 132) {//音量减
-                    var alarmVolume = audioManager.getStreamVolume(AudioManager.STREAM_ALARM)
-                    if(alarmVolume>0){
-                        Toast.makeText(this, "音量减", Toast.LENGTH_SHORT).show()
-                        audioManager.adjustSuggestedStreamVolume(
-                            AudioManager.STREAM_MUSIC, // 要调整的音频流类型
-                            AudioManager.ADJUST_LOWER, // 调整方向：增加音量
-                            0 // 可选的标志位，通常设置为0
-                        )
+                    else if (event.keyCode == 132) {//音量减
+                        //音量范围控制
+                        if(targetAlarmVolume>0){
+                            targetAlarmVolume--
+                        }
+                        if(targetMusicVolume>0){
+                            targetMusicVolume--
+                        }
+                        //Toast.makeText(this, "音量减", Toast.LENGTH_SHORT).show()
+                        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, targetAlarmVolume, 0)
+                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, targetMusicVolume, 0)
                         //return true // 如果事件已经被处理，返回 true
                     }
                 }
