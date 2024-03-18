c9b06b5630370c5869d947bbb71a1fc8b61e6d23
zhanshrd <zhanshrd@gmail.com>
2024-03-18 19:04:55 +0800

    device wake up volume recovery fix

--- app/src/main/java/com/zhanshrd/sidelauncher/volume/MyAccessibilityService.kt
+++ app/src/main/java/com/zhanshrd/sidelauncher/volume/MyAccessibilityService.kt
@@ -32,9 +32,31 @@ class MyAccessibilityService : AccessibilityService() {
                 var musicVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
 
                 if (musicVolume > 0 && alarmVolume == 0){
-                    //skip
+                    //符合该条件的情况：1、正常播放音乐中。2、小菱语音过程中。3、熄火再启动时。
+                    //只调整媒体音量，不调整闹钟音量
+                    var targetMusicVolume = musicVolume
+                    // 检查按键事件按键码
+                    if (event.keyCode == 131) {//音量加
+                        //音量范围控制
+                        if(targetMusicVolume<30){
+                            targetMusicVolume++
+                        }
+                        //Toast.makeText(this, "音量加", Toast.LENGTH_SHORT).show()
+                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, targetMusicVolume, 0)
+                        //return true // 如果事件已经被处理，返回 true
+                    }
+                    else if (event.keyCode == 132) {//音量减
+                        //音量范围控制
+                        if(targetMusicVolume>0){
+                            targetMusicVolume--
+                        }
+                        //Toast.makeText(this, "音量减", Toast.LENGTH_SHORT).show()
+                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, targetMusicVolume, 0)
+                        //return true // 如果事件已经被处理，返回 true
+                    }
                 }
                 else{
+                    //媒体音量和闹钟音量同时调整
                     var targetAlarmVolume = alarmVolume
                     var targetMusicVolume = musicVolume
 
